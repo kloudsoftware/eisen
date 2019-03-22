22c0f9dafda44e3c83da9a5bcd317bb4a3a5dbc6
Lars Grahmann <lars@kloud.software>
2019-03-22 02:23:58 +0100

    add dirty

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,7 @@ setTimeout(() => {
 
 setTimeout(() => {
     vRootDiv.attrs.push(new Attribute("id", "targetmod"));
+    //app.notifyDirty();
     let patch = renderer.diffAgainstLatest(app);
     patch(app.rootNode.htmlElement);
 }, 6000)
