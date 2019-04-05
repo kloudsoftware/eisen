26bddccd9b6913e7644fb9276f2e2e595cbfc88a
fred <fredlahde@gmail.com>
2019-04-05 19:36:48 +0200

    use microbundle

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-export { VApp } from './vdom/VApp'
-export { VNode, Attribute, VInputNode, VNodeType } from './vdom/VNode'
-export { Renderer } from './vdom/render';
-export { Props } from './vdom/Props';
-export { Component, ComponentHolder } from './vdom/Component';
-export { EventHandler } from './vdom/EventHandler';
-export { Router } from './Router';
+export * from './vdom';
+export * from './Router';
---
+++ src/vdom/index.ts
@@ -0,0 +1,7 @@
+export * from './Common';
+export * from './Component';
+export * from './EventHandler';
+export * from './Props';
+export * from './VApp';
+export * from './VNode';
+export * from './render';
