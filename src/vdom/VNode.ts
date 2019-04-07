62399fc90e8c188e93e69758e5feb8b8a7f038ed
fred <fredlahde@gmail.com>
2019-04-07 21:49:34 +0200

    Add jsdoc to VNode

--- src/vdom/VNode.ts
+++ src/vdom/VNode.ts
@@ -5,12 +5,26 @@ import { Props } from './Props';
 import { EvtHandlerFunc, EvtType } from './EventHandler';
 import { RouterLink } from '../Router';
 
+/**
+ * Attribute to identify a virtual node
+ */
 export const kloudAppId = "data-kloudappid";
 
+/**
+ * Possible type of a VNode
+ * See: {@link https://dev.w3.org/html5/html-author/#the-elements}
+ */
 export type VNodeType = '!--...--' | '!DOCTYPE ' | 'a' | 'abbr' | 'acronym' | 'address' | 'applet' | 'area' | 'article' | 'aside' | 'audio' | 'b' | 'base' | 'basefont' | 'bdi' | 'bdo' | 'big' | 'blockquote' | 'body' | 'br' | 'button' | 'canvas' | 'caption' | 'center' | 'cite' | 'code' | 'col' | 'colgroup' | 'data' | 'datalist' | 'dd' | 'del' | 'details' | 'dfn' | 'dialog' | 'dir' | 'div' | 'dl' | 'dt' | 'em' | 'embed' | 'fieldset' | 'figcaption' | 'figure' | 'font' | 'footer' | 'form' | 'frame' | 'frameset' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'head' | 'header' | 'hr' | 'html' | 'i' | 'iframe' | 'img' | 'input' | 'ins' | 'kbd' | 'label' | 'legend' | 'li' | 'link' | 'main' | 'map' | 'mark' | 'meta' | 'meter' | 'nav' | 'noframes' | 'noscript' | 'object' | 'ol' | 'optgroup' | 'option' | 'output' | 'p' | 'param' | 'picture' | 'pre' | 'progress' | 'q' | 'rp' | 'rt' | 'ruby' | 's' | 'samp' | 'script' | 'section' | 'select' | 'small' | 'source' | 'span' | 'strike' | 'strong' | 'style' | 'sub' | 'summary' | 'sup' | 'svg' | 'table' | 'tbody' | 'td' | 'template' | 'textarea' | 'tfoot' | 'th' | 'thead' | 'time' | 'title' | 'tr' | 'track' | 'tt' | 'u' | 'ul' | 'var' | 'video' | 'wbr';
 
+/**
+ * Event firing when the Node appears on the DOM and such gets a HTMLElement
+ */
 export type OnDomEvent = (html: HTMLElement) => void;
-
+function b(a) { return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b) }
+/**
+ * A VNode represents a virtual DOM Node. It's the main data structure to represent a virtual DOM
+ * It contains all data to render a real DOM node
+ */
 export class VNode implements Comparable<VNode> {
     app: VApp;
     id: string;
@@ -26,7 +40,27 @@ export class VNode implements Comparable<VNode> {
     modifiedInnerHtml = false;
     onDomEvenList = new Array<OnDomEvent>();
 
-    constructor(app: VApp, nodeName: VNodeType, children: VNode[], innerHtml?: string, props?: Props, attrs?: Attribute[], parent?: VNode, id?: string) {
+    /**
+     * Creates a new VNode. Typically not directly called, but through `app.k` or `appendChild`
+     * @param app reference to the VApp instance
+     * @param nodeName tag name of the node. Must be a valid HTML tag name
+     * @param children Array containing childs of this node, if any
+     * @param innerHtml the innerHTML of the node
+     * @param props the props of this node, see {@link Props}
+     * @param attrs attributes of this node, see {@link Attribute}
+     * @param parent the parent node, if any
+     * @param id the UUID of this node, to refer to it within the virtual dom. Different from the id in attributes
+     */
+    constructor(
+        app: VApp,
+        nodeName: VNodeType,
+        children: VNode[],
+        innerHtml?: string,
+        props?: Props,
+        attrs?: Attribute[],
+        parent?: VNode,
+        id?: string
+    ) {
         if (attrs == undefined) {
             this.attrs = new Array();
         } else {
@@ -56,8 +90,37 @@ export class VNode implements Comparable<VNode> {
         this.attrs.push(new Attribute(kloudAppId, this.id));
     }
 
+    /**
+     * Sets the HTMLElement of this node, that is the real DOM node
+     * Triggers the onDomEvents of this node
+     * @param el the real DOM element of this node
+     */
+    public setHtmlElement(el: HTMLElement) {
+        this.htmlElement = el;
+        this.onDomEvenList.forEach(f => f(el));
+    }
+
+    /**
+     * Add an eventhandler that runs when this node becomes a HTMLElement assigned
+     * Contains shortcut to run the handler immediately if a HTMLElement is already assigned
+     * @param func the handler to run
+     */
+    public addOnDomEventOrExecute(func: OnDomEvent) {
+        if (this.htmlElement == undefined) {
+            this.onDomEvenList.push(func)
+            return;
+        }
+
+        func(this.htmlElement);
+    }
+
+    /**
+     * Add a onFocus listener
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onfocus}
+     * @param func handler to be run
+     */
     public addFocusListener(func: EvtHandlerFunc) {
-        if(this.htmlElement == undefined) {
+        if (this.htmlElement == undefined) {
             this.onDomEvenList.push((el) => {
                 el.addEventListener("focus", func);
             });
@@ -67,22 +130,13 @@ export class VNode implements Comparable<VNode> {
         this.htmlElement.addEventListener("focus", func);
     }
 
-    public setHtmlElement(el: HTMLElement) {
-        this.htmlElement = el;
-        this.onDomEvenList.forEach(f => f(el));
-    }
-
-    public addOnDomEventOrExecute(func: OnDomEvent) {
-        if(this.htmlElement == undefined) {
-            this.onDomEvenList.push(func)
-            return;
-        }
-
-        func(this.htmlElement);
-    }
-
+    /**
+     * Add a onBlur listener
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onblur}
+     * @param func handler to be run
+     */
     public addBlurListener(func: EvtHandlerFunc) {
-        if(this.htmlElement == undefined) {
+        if (this.htmlElement == undefined) {
             this.onDomEvenList.push((el) => {
                 el.addEventListener("blur", func);
             });
@@ -92,6 +146,12 @@ export class VNode implements Comparable<VNode> {
         this.htmlElement.addEventListener("blur", func);
     }
 
+    /**
+     * Adds an attribute to the node
+     * replaces the value if an attribute with the given name is already found
+     * @param name name of the attribute
+     * @param value value of the attribute
+     */
     public setAttribute(name: string, value: string) {
         const isSet = this.attrs.filter(a => a.attrName == name).length > 0;
 
@@ -103,43 +163,83 @@ export class VNode implements Comparable<VNode> {
         this.attrs.filter(a => a.attrName == name)[0].attrValue = value;
     }
 
+    /**
+     * Get the childrens of this node
+     * Handle with care, changes made to them via this function will not be tracked by the renderer
+     */
     public $getChildren() {
-        if(this.children.length != 0 && this.children[0] != undefined &&  this.children[0].nodeName == "img") {
+        if (this.children.length != 0 && this.children[0] != undefined && this.children[0].nodeName == "img") {
             //console.trace()
         }
         return this.children;
     }
 
+    /**
+     * Sets the innerHtml of this node
+     * Using this function marks the app as dirty
+     * @param str the new innerHtml
+     */
     public setInnerHtml(str: string) {
         this.app.notifyDirty();
         this.modifiedInnerHtml = true;
         this.innerHtml = str;
     }
 
+    /**
+     * Sets the innerHtml of this node
+     * handle with care, changes will not be tracked by the renderer
+     * @param str the new innerHtml
+     */
     public $setInnerHtmlNoDirty(str: string) {
         this.modifiedInnerHtml = true;
         this.innerHtml = str;
     }
 
+    /**
+     * Returns the parsed innerHtml of this node
+     * That is, all placeholders will be replaced with the corresponding props
+     */
     public getInnerHtml(): string {
         return new Stringparser().parse(this.innerHtml, this.props);
     }
 
+    /**
+     * Replace a child of this node with a new one
+     * Using this function marks the app as dirty
+     * @param old the child that will be replaced
+     * @param node the child to replace with
+     */
     public replaceChild(old: VNode, node: VNode) {
         this.replaceWith(old, node);
     }
 
+    /**
+     * Remove a child of this node
+     * Using this function marks the app as dirty
+     * @param toRemove the child to remove
+     */
     public removeChild(toRemove: VNode) {
         this.app.notifyDirty();
         this.replaceWith(toRemove, undefined);
     }
 
+    /**
+     * Append a child to this node
+     * Using this function marks the app as dirty
+     * @param node the child to add
+     */
     public appendChild(node: VNode) {
         this.app.notifyDirty();
         node.parent = this;
         this.children.push(node);
     }
 
+    /**
+     * Add a listener to a given event, targeting this node
+     * See {@link EventHandler}
+     * @param evt the type of the event
+     * @param func the handler to be run
+     */
     public addEventlistener(evt: EvtType, func: EvtHandlerFunc) {
         this.app.eventHandler.registerEventListener(evt, func, this);
     }
@@ -160,6 +260,11 @@ export class VNode implements Comparable<VNode> {
         }
     }
 
+    /**
+     * Clones this node, returning a new identical node that was deep copied
+     * Works recursively on child nodes
+     * @param parent the parent node from where to clone from
+     */
     public clone(parent: VNode): VNode {
         const id = this.id;
         const nodeName = this.nodeName;
@@ -185,7 +290,12 @@ export class VNode implements Comparable<VNode> {
         return clonedNode;
     }
 
-    //Sets a new id for every item
+    /**
+     * Clones this node, returning a new identical node that was deep copied
+     * Same as clone(), but sets an new id for every node
+     * Works recursively on child nodes
+     * @param parent the parent node from where to clone from
+     */
     public copy(parent: VNode): VNode {
         const id = uuid();
         const nodeName = this.nodeName;
@@ -202,12 +312,22 @@ export class VNode implements Comparable<VNode> {
         clonedNode.htmlElement = htmlElement;
         return clonedNode;
     }
+
+    /**
+     * Compares this node to another node
+     * @param o the other node to compare
+     */
     equals(o: VNode): boolean {
         if (o == undefined) return false;
 
         return this.nodeName == o.nodeName;
     }
 
+    /**
+     * Adds the given class[es] to the node
+     * Calling this multple times appends classes
+     * @param name the class name to add
+     */
     public addClass = (name: string) => {
         this.app.notifyDirty();
         let classAttr = this.attrs.filter(el => el.attrName == "class")[0];
@@ -221,6 +341,10 @@ export class VNode implements Comparable<VNode> {
         return this;
     }
 
+    /**
+     * Checks if a given class is set on this node
+     * @param name the name of the class to look for
+     */
     public hasClass = (name: string): boolean => {
         const classAttr = this.attrs.filter(el => el.attrName == "class")[0];
 
@@ -231,6 +355,10 @@ export class VNode implements Comparable<VNode> {
         return classAttr.attrValue.indexOf(name) != -1;
     }
 
+    /**
+     * Removes the given class if it's set
+     * @param name the name of the class to remove
+     */
     public removeClass = (name: string) => {
         this.app.notifyDirty();
         const classAttr = this.attrs.filter(el => el.attrName == "class")[0];
@@ -243,6 +371,9 @@ export class VNode implements Comparable<VNode> {
         return this;
     }
 
+    /**
+     * Returns a new {@link VNodeBuilder} to build a VNode
+     */
     public static builder(): VNodeBuilder {
         return new VNodeBuilder();
     }
@@ -390,13 +521,13 @@ export class VInputNode extends VNode {
         return !this.validationFuncs.map(holder => {
             let result = holder[0](shouldNotifyOthers);
 
-            if(result == false) {
-                if(this.hasClass(holder[1])) {
+            if (result == false) {
+                if (this.hasClass(holder[1])) {
                     return result;
                 }
                 this.hasError = true;
                 this.addClass(holder[1])
-            } else if(this.hasClass(holder[1])) {
+            } else if (this.hasClass(holder[1])) {
                 this.removeClass(holder[1])
             }
 
@@ -406,7 +537,7 @@ export class VInputNode extends VNode {
 
     validate(validateFunc: ValidationFunc, errorClass: string) {
         this.validationFuncs.push([validateFunc, errorClass])
-        if(this.hasValidateBlurFunction) {
+        if (this.hasValidateBlurFunction) {
             return;
         }
 
