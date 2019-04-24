8cd5959c4f1c0b69d8e05452a913facf00766bbc
Lars Grahmann <lgrahm@technik-emden.de>
2019-04-24 15:13:49 +0200

    add async router implementation (#5)
    * add async router implementation
    
    * remove console trace
    
    * change returntype on fail to false
    
    * add catch to resolve route
    
    * move interfaces
    
    * Return router from useRouter, fix error handling
    
    - Simplify resolveRoute: Remove wrapping Promise, use static functions for
    resolve \ reject
    - Better error message when no route is found to route to
    - return router instance from useRouter / useCustomRouter
    
    * Remove unused import
    
    * add return types to useRouter functions

--- src/Router.ts
+++ src/Router.ts
@@ -5,19 +5,31 @@ import { Props } from './vdom/Props';
 
 type ComponentPropsHolder = [Component, Props];
 
-export class Router {
+export interface IRouter {
+    resolveRoute(path: string): Promise<boolean>;
+    registerRoute(path: string, component: Component, props?: Props): void;
+    hasRouteRegistered(path: string): boolean;
+}
+
+export interface MiddleWare {
+    check(path: string): Promise<boolean>
+}
+
+export class Router implements IRouter {
     private app: VApp;
     private resolvedRouterMap: Map<string, ComponentHolder> = new Map<string, ComponentHolder>();
     public componentMap: Map<string, ComponentPropsHolder> = new Map();
     private mount: VNode;
     private currPath: string = undefined;
+    private middleWares: Array<MiddleWare> = new Array<MiddleWare>();
 
     constructor(app: VApp, mount: VNode) {
         this.mount = mount;
         this.app = app;
 
         window.onpopstate = (event) => {
-            this.resolveRoute(document.location.pathname)}
+            this.resolveRoute(document.location.pathname)
+        }
     }
 
     registerRoute(path: string, component: Component, props?: Props) {
@@ -31,30 +43,31 @@ export class Router {
         return this.componentMap.has(path);
     }
 
-    resolveRoute(path: string): boolean {
-        history.replaceState(null, "", path)
+    resolveRoute(path: string): Promise<boolean> {
+        return Promise.all(this.middleWares.map(it => it.check(path))).then(() => {
+            history.replaceState(null, "", path)
 
-        if (this.currPath == path) {
-            return true;
-        }
+            if (this.currPath == path) {
+                return Promise.resolve(true);
+            }
 
-        if (this.resolvedRouterMap.has(path)) {
+            if (this.resolvedRouterMap.has(path)) {
+                this.mount.$getChildren().forEach(child => this.mount.removeChild(child));
+                this.app.remountComponent(this.resolvedRouterMap.get(path) as ComponentHolder, this.mount);
+                this.currPath = path;
+                return Promise.resolve(true);
+            }
+
+            if (!this.componentMap.has(path)) {
+                return Promise.reject("No component registered with the router for " + path + " register one using 'registerRoute()'");
+            }
+
             this.mount.$getChildren().forEach(child => this.mount.removeChild(child));
-            this.app.remountComponent(this.resolvedRouterMap.get(path) as ComponentHolder, this.mount);
             this.currPath = path;
-            return true;
-        }
-
-        if (!this.componentMap.has(path)) {
-            console.error("No component registered with the router for ", path)
-            return false;
-        }
-
-        this.mount.$getChildren().forEach(child => this.mount.removeChild(child));
-        this.currPath = path;
-        let cmp = this.componentMap.get(path);
-        this.resolvedRouterMap.set(path, this.app.routerMountComponent(cmp[0], this.mount, cmp[1]));
-        return true;
+            let cmp = this.componentMap.get(path);
+            this.resolvedRouterMap.set(path, this.app.routerMountComponent(cmp[0], this.mount, cmp[1]));
+            return Promise.resolve(true);
+        });
     }
 }
 
@@ -71,9 +84,9 @@ export class RouterLink extends VNode {
 
     clickFunction(event: Event, link: VNode) {
         const ln = link as RouterLink;
-        if (ln.app.router.componentMap.has(ln.target)) {
+        if (ln.app.router.hasRouteRegistered(ln.target)) {
             history.pushState({}, "", document.location.pathname)
-            ln.app.router.resolveRoute(ln.target);
+            ln.app.router.resolveRoute(ln.target).catch(err => console.error("Error occured in routing: ", err));
             event.preventDefault();
             return;
         }
